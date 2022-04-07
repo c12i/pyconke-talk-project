fd92e0f601120314c4ab23f854e03214e0a8b5fa
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-04-07 11:27:06 +0300

    Add multiprocessing example

---
+++ palingrams_py/palingrams_multiprocessing.py
@@ -0,0 +1,30 @@
+from math import ceil 
+from multiprocessing import Pool
+import os
+import time
+
+import utils
+
+source_word_list = utils.load_file('./words.txt')
+
+def find_palingrams(word_list_chunk):
+    palingram_list = []
+    for word in word_list_chunk:
+        word_len = len(word)
+        rev_word = word[::-1]
+        if word_len > 1:
+            for idx in range(word_len):
+                if word[idx:] == rev_word[:word_len-idx] and rev_word[word_len-idx:] in source_word_list:
+                    palingram_list.append((word, rev_word[word_len-idx:]))
+                if word[:idx] == rev_word[word_len-idx:] and rev_word[:word_len-idx] in source_word_list:
+                    palingram_list.append((word, rev_word[:word_len-idx]))
+    return palingram_list
+    
+
+if __name__ == '__main__':
+	chunk_size = ceil(len(source_word_list) / os.cpu_count()) 
+	chunks = list(utils.split_list_to_chunks(source_word_list, chunk_size))
+	start_time = time.time()
+	with Pool(os.cpu_count()) as p:
+		print(p.map(find_palingrams, chunks))
+	print(f"Runtime for this program was: {time.time() - start_time} s")
--- palingrams_py/utils.py
+++ palingrams_py/utils.py
@@ -1,4 +1,17 @@
+import sys
+
+def load_file(file_path):
+    try:
+        with open(file_path) as in_file:
+            loaded_txt = in_file.read().strip().split("\n")
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return loaded_txt
+    except IOError as e:
+        print(f"{e}\nError opening {file_path}", file=sys.stderr)
+        sys.exit(1)
+
+source_word_list = load_file('./words.txt')
+
 def split_list_to_chunks(l, n):
-    # looping till length l
     for i in range(0, len(l), n): 
         yield l[i:i + n]
