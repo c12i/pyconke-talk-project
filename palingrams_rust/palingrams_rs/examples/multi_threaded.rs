9ef6d04ebe39521c631221bd31958391566bc2dc
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-04-07 14:54:29 +0300

    Organize rust project to workspaces

---
+++ palingrams_rust/palingrams_rs/examples/multi_threaded.rs
@@ -0,0 +1,12 @@
+use std::time::Instant;
+use palingrams_rs::{find_palingrams_concurrent, load_file_data};
+
+fn main() {
+	let word_list = load_file_data("./words.txt");
+	let start = Instant::now();
+	let result = find_palingrams_concurrent(word_list);
+	println!("{:?}", result);
+	println!("total palingrams = {}", result.len());
+	let duration = start.elapsed();
+	println!("Runtime for this program was: {:?}", duration);
+}
