2031dc66f96d0968dfa6b343d8d2030619c26c20
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-04-30 17:46:02 +0300

    Update examples

--- rust/palingrams_cpython/examples.py/test.py
+++ rust/palingrams_cpython/examples.py/test.py
@@ -1,14 +1,12 @@
 import time
+from palingrams_cpython import find_palingrams_concurrent
 
-import palingrams_cpython
 import utils
 
 if __name__ == '__main__':
 	w = utils.load_file('./words.txt')
 	start_time = time.time()
-	palingrams = palingrams_cpython.find_palingrams_concurrent(w)
-	# palingrams = palingrams_cpython.find_palingrams(w) <-- uncomment this and comment out above code to try out single threaded variant
+	palingrams = find_palingrams_concurrent(w)
 	end_time = time.time()
-	print(palingrams)
 	print(f"Runtime for this program was: {end_time - start_time} s")
 	print(f"result count = {len(palingrams)}")
--- rust/palingrams_pyo3/examples.py/test.py
+++ rust/palingrams_pyo3/examples.py/test.py
@@ -1,13 +1,12 @@
 import time
+from palingrams_pyo3 import find_palingrams_concurrent
 
-import palingrams_pyo3
 import utils
 
 if __name__ == '__main__':
 	w = utils.load_file('./words.txt')
 	start_time = time.time()
-	palingrams = palingrams_pyo3.find_palingrams_concurrent(w)
-	# palingrams = palingrams_pyo3.find_palingrams(w) 
+	palingrams = find_palingrams_concurrent(w)
 	end_time = time.time()
 	print(f"Runtime for this program was: {end_time - start_time} s")
 	print(f"result count = {len(palingrams)}")
