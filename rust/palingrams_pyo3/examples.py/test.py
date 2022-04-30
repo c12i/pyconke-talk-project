4be6d24f9943fa029de77c6761077817a6b34940
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-04-30 17:31:22 +0300

    Update pyo3 example

--- rust/palingrams_pyo3/examples.py/test.py
+++ rust/palingrams_pyo3/examples.py/test.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 	w = utils.load_file('./words.txt')
 	start_time = time.time()
 	palingrams = palingrams_pyo3.find_palingrams_concurrent(w)
-	# palingrams = palingrams_cpython.find_palingrams(w) <-- uncomment this and comment out above code to try out single threaded variant
+	# palingrams = palingrams_pyo3.find_palingrams(w) 
 	end_time = time.time()
 	print(f"Runtime for this program was: {end_time - start_time} s")
 	print(f"result count = {len(palingrams)}")
